93e479110f850c618bfd9b5bd6c29d1f5d05cecd
Rohit Sharma <sharmarohit469494@gmail.com>
2026-05-24 16:25:55 +0530

    variable & datatypes

---
+++ Variable_DataTypes_003/Variable.java
@@ -0,0 +1,14 @@
+package JavaByHarry.Variable_DataTypes_003;
+
+public class Variable {
+    public static void main(){
+        //Write a program to add 3 numbers
+
+        int num1 = 5;
+        int num2 = 10;
+        int num3 = 5;
+
+        int result = num1 + num2 + num3;
+        System.out.println(result);
+    }
+}
