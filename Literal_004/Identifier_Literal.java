0bff8f125e166d41f881c44b85ea1ca8dd0c287d
Rohit Sharma <sharmarohit469494@gmail.com>
2026-05-24 16:31:45 +0530

    Identifier and Literal

---
+++ Literal_004/Identifier_Literal.java
@@ -0,0 +1,23 @@
+package JavaByHarry.Literal_004;
+
+public class Identifier_Literal {
+    //    Literal = Koi constant value jo assign ho sake koi variable mein user literal kehenge
+//    Identifier = Koi bhi variable ke name ko identifier bolenge
+    static void main() {
+
+        byte age = 34;
+        int age2 = 55;
+        short age3 = 7;
+        char ch = 'A';
+        long dinoAge = 34343434324239l;
+        float fl = 3.4f;
+        boolean isTrue = false;
+        double db = 34.4343;
+        String str = "Harry";
+
+
+        System.out.println(str);
+    }
+
+
+}
