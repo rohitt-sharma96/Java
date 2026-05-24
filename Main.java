52df4b617ef68cca081260bc0f6bac664e209673
Rohit Sharma <sharmarohit469494@gmail.com>
2026-05-24 16:19:31 +0530

    First Program

---
+++ Main.java
@@ -0,0 +1,11 @@
+package JavaByHarry;
+
+class Main{
+    public static void main() {
+        System.out.println("Hello World");
+    }
+}
+
+//Naming Convention
+// HelloIndia --> PascalConvention (class)
+// helloIndia --> camelConvention  (function)
