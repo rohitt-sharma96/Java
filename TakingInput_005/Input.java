89858d209372a36410e853380487643ced148b03
Rohit Sharma <sharmarohit469494@gmail.com>
2026-05-24 17:04:02 +0530

    taking input from user

---
+++ TakingInput_005/Input.java
@@ -0,0 +1,56 @@
+package JavaByHarry.TakingInput_005;
+import java.util.Scanner;
+
+
+public class Input {
+    static void main() {
+
+        Scanner sc = new Scanner(System.in);
+
+//Adding to Integer value
+/*
+        System.out.print("Enter Number 1: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter Number 2: ");
+        int num2 = sc.nextInt();
+
+        int sum = num1 + num2;
+        System.out.println(sum);
+*/
+
+//Adding to Floating value
+/*
+
+
+
+        System.out.print("Enter float value 1: ");
+         float a = sc.nextFloat();
+
+        System.out.print("Enter float value 2: ");
+        float b = sc.nextFloat();
+
+         float sum2 = a+b;
+      System.out.println(sum2);
+
+  */
+
+//Check Input Number is Integer or not
+/*        // System.out.print("Enter the Floating Value :");
+        boolean isNum = sc.hasNextInt();
+        System.out.println(isNum);
+*/
+
+        //Taking String from User
+/*        System.out.print("Enter Your String :");
+//        String  word = sc.next();    //One Word
+        String str = sc.nextLine();  // full String
+        System.out.println(str);
+*/
+
+
+
+
+
+    }
+
+}
