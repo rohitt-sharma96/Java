604620e21654058fc3be2be05ea20d03afe4875e
Rohit Sharma <sharmarohit469494@gmail.com>
2026-07-13 00:39:43 +0530

    Precedence & Associativity (I.M.P)

---
+++ J_009_Precedence/Precedence.java
@@ -0,0 +1,24 @@
+package JavaByHarry.J_009_Precedence;
+
+public class Precedence {
+    static void main(String[] args) {
+        //Precedence and Associativity
+        /*
+        Precedence - Jis operator ka precedence jyada ho wo pehle evaluate hoga
+        e.g - 3 * 2 - 4
+            = 6 - 4  (* precedence jyada hai - se )
+            = 2
+
+        Associativity -  Jb do operator ke precedence baraabar ho to
+                        Associativity dekhi jaati hai to (left to right || right to left)
+                        evaluate hote hai. (Associativity rule ke according)
+
+                        e.g.  = 60/6 - 3 * 6 (yaha / and * dono ki precedence same hai)
+                                              to ab left to right evaluate hoga ( according to rule)
+
+                              = 10 - 3 * 6
+                              = 10 - 18
+                              = -8
+         */
+    }
+}
