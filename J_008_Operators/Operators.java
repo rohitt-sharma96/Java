a19faee777b194be22a1e88260c31b8da56a4d2a
Rohit Sharma <sharmarohit469494@gmail.com>
2026-07-13 00:09:58 +0530

    Operators

---
+++ J_008_Operators/Operators.java
@@ -0,0 +1,25 @@
+package JavaByHarry.J_008_Operators;
+
+public class Operators {
+    static void main(String[] args) {
+        //Arithmetic Operator
+        int a = 10;
+        int b = a % 5; //Module Operator
+
+        //Comparison Operator
+//        System.out.println(5 == 10);
+//        System.out.println(5<10);
+
+        //Logical Operator
+        System.out.println(5 ==5 && 10 > 2);
+        System.out.println(2>6 || 2==2);
+
+       //Bitwise Operator
+        System.out.println(10&11);
+//        10
+//      & 11
+//      --------
+//        10
+
+    }
+    }
